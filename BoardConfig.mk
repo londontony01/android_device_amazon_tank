c03056612ad6d1d98d8761425b346bc773fe62cb
A2L5E0X1 <alexed25@protonmail.com>
2021-06-19 22:14:20 +0200

    [DNM] tank: set selinux to permissive
    Change-Id: I92b09d49d477d390bf59d73bb19b3e719772e6dc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ USE_OPENGL_RENDERER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x4000000 --second_offset 0x00f00000 --tags_offset 0x00000100
